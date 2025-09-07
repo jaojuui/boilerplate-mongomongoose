169b9f13d20560ba5a864a888aad36e7ddeb501d
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 23:01:11 +0700

    update s1

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose'); 
 
-
+mongoose.connect(MONGO_UR, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
