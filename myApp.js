0986a2ed1c4138e4e49ff7615f6cdb82d549a6e4
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 23:32:21 +0700

    update s1

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
+.then(() => console.log('✅ MongoDB Connected'))
+.catch((err) => console.error('❌ MongoDB Connection Error:', err));
 
 let Person;
 
