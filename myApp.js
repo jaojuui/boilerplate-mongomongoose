1d3949e474ed1c69046eef375c9f3c54ba1fa204
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-08 14:35:17 +0700

    update s10

--- myApp.js
+++ myApp.js
@@ -93,8 +93,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name:nameToRemove},function (err,data){
+    if(err) console.log(err);
+    done(null,data)
+  })
 };
 
 const queryChain = (done) => {
