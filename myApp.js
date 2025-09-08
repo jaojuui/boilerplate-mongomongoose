c6a9a7e95f956783134590c38b19436c17fb81ba
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-08 14:29:50 +0700

    update s8

--- myApp.js
+++ myApp.js
@@ -78,14 +78,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
 
   const ageToSet = 20;
-  Person.findOneAndUpdate({name:personName},function (err,data){
+  Person.findOneAndUpdate({name:personName},{age:ageToSet},{new:true},function (err,data){
     if(err) console.log(err);
-    data.age = ageToSet;
-    data.save(function (err,data){
-      if(err) console.error(err);
-      done(null ,data);
-    })
-  },{new:true});
+    done(null,data)
+  });
 };
 
 const removeById = (personId, done) => {
